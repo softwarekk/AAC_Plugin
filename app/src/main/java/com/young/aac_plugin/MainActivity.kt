f24227ce6800d689167e5f910678ae89a0bc82c0
闫永凯 <youngyan@beile.com>
2020-07-20 10:56:50 +0800

    master

---
+++ app/src/main/java/com/young/aac_plugin/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.young.aac_plugin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.young.aac_plugin.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/young/aac_plugin/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.young.aac_plugin.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
